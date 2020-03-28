6bc6477d50f152e32eaf6032f17cea68226f057c
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-03-28 21:03:25 +0530

    Updates

---
+++ precache-manifest.917c50102609361f5b9b67834eb15323.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dc254c4cecf9642409d6f2391c571d",
+    "url": "/ReactRobofriendsApp/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/ReactRobofriendsApp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c2f8222de2e6cbca6428",
+    "url": "/ReactRobofriendsApp/static/css/main.c4d3b898.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/ReactRobofriendsApp/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRobofriendsApp/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f8222de2e6cbca6428",
+    "url": "/ReactRobofriendsApp/static/js/main.0e31477a.chunk.js"
+  },
+  {
+    "revision": "d2094b1a2fcaff070870",
+    "url": "/ReactRobofriendsApp/static/js/runtime-main.c640d222.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactRobofriendsApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
